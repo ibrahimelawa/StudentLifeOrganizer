0c92040eaaebf61957b29fdfd9b925255a13dd57
abedjaper <abed.japer.xz@gmail.com>
2025-05-01 18:30:54 +0300

    update

---
+++ app/src/main/java/com/example/studentlifeorganizer/OnboardingActivity.java
@@ -0,0 +1,38 @@
+package com.example.studentlifeorganizer;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class OnboardingActivity extends AppCompatActivity {
+    private int currentScreen = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_one);
+
+        findViewById(R.id.next_button).setOnClickListener(v -> navigateToNextScreen());
+    }
+
+    private void navigateToNextScreen() {
+        currentScreen++;
+        switch (currentScreen) {
+            case 1:
+                setContentView(R.layout.screen_two);
+                findViewById(R.id.next_button).setOnClickListener(v -> navigateToNextScreen());
+                break;
+            case 2:
+                setContentView(R.layout.screen_three);
+                findViewById(R.id.next_button).setOnClickListener(v -> navigateToNextScreen());
+                break;
+            default:
+                Intent intent = new Intent(OnboardingActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+}
